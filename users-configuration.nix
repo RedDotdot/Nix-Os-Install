734491bf264c9487c1e0be05b93f5687d127ec9d
RedDot <49599382+RedDotdot@users.noreply.github.com>
2025-05-08 23:05:45 +0200

    Create users-configuration.nix

---
+++ users-configuration.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{ 
+  users.users.alexy = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      brave
+    ];
+  }
+}
