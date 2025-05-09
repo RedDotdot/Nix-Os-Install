3b93ede6b083f904bcf3d3caebfd41e89ccb61b1
RedDot <49599382+RedDotdot@users.noreply.github.com>
2025-05-09 14:18:22 +0200

    Update boot-configuration.nix

--- boot-configuration.nix
+++ boot-configuration.nix
@@ -3,10 +3,10 @@
 {
   boot.initrd.kernelModules = [ "tpm" "tpm_tis" "tpm_crb" ];
   boot.initrd.luks.devices."swap".device = "/dev/disk/by-label/LUKSSWAP";
-  boot.initrd.availablePackages = with pkgs; [
-    clevis
-    tpm2-tools
-  ];
+  boot.initrd.extraUtilsCommands = ''
+    copy_bin_and_libs ${pkgs.clevis}/bin/clevis
+    copy_bin_and_libs ${pkgs.tpm2-tools}/bin/tpm2_unseal
+  '';
   boot.initrd.preLVMCommands = ''
     echo "Attempting to auto-unlock root via clevis"
     clevis luks unlock -d /dev/disk/by-label/LUKSROOT -n root || echo "Clevis unlock failed"
